87561ee22cec3d5e06e85cd70722a0c3afed778e
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2021-04-08 18:26:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Polyline,
+  Popup
+} from "react-leaflet";
+import { usePosition } from "./use-location";
+
+export default function App() {
+  const [trackData, setTrackData] = useState([]);
+  const [polygonData, setPolygonData] = useState();
+  const initialPosition = [19.075984, 72.877656];
+  const [isTracking, setIsTracking] = useState(false);
+  const { position } = usePosition(isTracking);
+
+  useEffect(() => {
+    if (isTracking && position) {
+      setTrackData([...trackData, position]);
+    }
+  }, [isTracking, position]);
+
+  const finishPolygon = () => {
+    setPolygonData([...trackData, trackData[0]]);
+    setTrackData([]);
+  };
+
+  return (
+    <div>
+      {isTracking ? "tracking" : "not tracking"}
+      <button onClick={() => setIsTracking(true)}>start</button>
+      <button onClick={() => setIsTracking(false)}>stop</button>
+      <button onClick={finishPolygon}>render as polygon</button>
+      <MapContainer center={initialPosition} zoom={6} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polyline pathOptions={{ color: "blue" }} positions={trackData} />
+        {polygonData && (
+          <Polyline pathOptions={{ color: "purple" }} positions={polygonData} />
+        )}
+      </MapContainer>
+    </div>
+  );
+}
